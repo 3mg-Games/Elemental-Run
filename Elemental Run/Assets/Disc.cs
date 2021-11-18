c78bc0484cac0af8cd1aa5d809b1813cb91e6d5e
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-11-18 16:49:43 +0530

    spinning disc added

---
+++ Elemental Run/Assets/Disc.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc : MonoBehaviour
+{
+    [SerializeField] GameObject path;
+    [SerializeField] float movementSpeed = 5f;
+
+    GameSession gameSession;
+
+    int wayPointsIndex;
+    int wayPointsCount;
+
+    List<Transform> waypoints = new List<Transform>();
+
+    Vector3 initialPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+        wayPointsIndex = 0;
+        wayPointsCount = path.transform.childCount;
+        for(int i = 0; i < wayPointsCount; i++)
+        {
+            waypoints.Add(path.transform.GetChild(i));
+        }
+
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wayPointsIndex <= wayPointsCount - 1)
+        {
+            Vector3 dir;
+
+            //verticalInput = 1;
+            //transform.forward = new Vector3(-verticalInput, 0, Mathf.Abs(verticalInput) - 1);
+
+            var targetPosition = waypoints[wayPointsIndex].transform.position;
+
+            var movementThisFrame = movementSpeed * Time.deltaTime;
+            //var rotationThisFrame = turnRotateSpeed * Time.deltaTime;
+            dir = targetPosition - transform.position;
+           // Quaternion rotation = Quaternion.LookRotation(-dir, Vector3.up);
+            //Debug.Log(rotation);
+
+            transform.position = Vector3.MoveTowards        //try character controller here
+                      (transform.position, targetPosition, movementThisFrame);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationThisFrame);
+
+
+            if (transform.position == targetPosition)
+                wayPointsIndex++;
+
+
+        }
+
+        else
+        {
+            transform.position = initialPos;
+            wayPointsIndex = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(gameSession.Kill());
+
+        }
+    }
+}
