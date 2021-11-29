b1a8a4683c3ed6f22ec93c7c9742694067466faf
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-11-29 15:37:37 +0530

    new spray vfx added

---
+++ Elemental Run/Assets/Game/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] AudioClip pickUpSfx;
+    [SerializeField] [Range(0, 1f)] float pickUpSfxVolume = 1f;
+
+    GameSession gameSession;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameSession == null)
+        {
+            gameSession = FindObjectOfType<GameSession>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(pickUpSfx,
+                   Camera.main.transform.position,
+                   pickUpSfxVolume);
+            gameSession.IncrementCoin();
+            Destroy(gameObject);
+
+        }
+    }
+}
