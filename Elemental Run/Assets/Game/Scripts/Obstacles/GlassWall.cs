39d5381cbe54ef4e29094cac00e8f7376e629ded
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-12-10 13:11:51 +0530

    glass wall added

--- Elemental Run/Assets/Game/Scripts/Obstacles/GlassWall.cs
+++ Elemental Run/Assets/Game/Scripts/Obstacles/GlassWall.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GlassWall : MonoBehaviour
 {
+    [SerializeField] GameObject glassWallBrokenPrefab;
+    [SerializeField] float timeAfterWhichPiecesDisappear = 1.5f;
+    [SerializeField] float explosionForce = 5f;
+    [SerializeField] float explosionRadius = 5f;
+
+    PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,18 @@ public class GlassWall : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            GameObject glassWallBroken = Instantiate(glassWallBrokenPrefab,
+                transform.position,
+                transform.rotation);
+
+            foreach(Transform child in glassWallBroken.transform)
+            {
+                Rigidbody rb = child.gameObject.GetComponent<Rigidbody>();
+                rb.AddExplosionForce(explosionForce,
+                    player.transform.position,
+                    explosionRadius);
+                Destroy(child.gameObject, timeAfterWhichPiecesDisappear);
+            }
             Destroy(gameObject);
         }
     }
